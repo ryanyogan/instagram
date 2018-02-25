ca84eb36bb8d375fb08145e130d334969edfebf3
Ryan Yogan <ryanyogan@gmail.com>
2018-02-24 22:03:51 -0600

    Completed the first pass of the <PhotoCard /> component

---
+++ src/components/PhotoCard/Meta.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { human, systemWeights } from 'react-native-typography';
+
+const styles = StyleSheet.create({
+  root: {
+    minHeight: 50,
+    paddingHorizontal: 16,
+  },
+  wrapper: {
+    flex: 1,
+  },
+  text: {
+    ...human.footnoteObject,
+    ...systemWeights.light,
+  },
+  regularText: {
+    ...systemWeights.regular,
+  },
+});
+
+const Meta = ({
+  caption = 'this is our Narhwal Bella!',
+  username = 'mryogibeara',
+}) => (
+  <View style={styles.root}>
+    <View style={styles.wrapper}>
+      <Text style={styles.text}>
+        Liked by <Text style={styles.regularText}>Jack Ryan</Text> and{' '}
+        <Text style={styles.regularText}>1,288,341 others.</Text>
+      </Text>
+    </View>
+    <View style={styles.wrapper}>
+      <Text style={styles.text}>
+        <Text style={styles.regularText}>{username}</Text> {caption}
+      </Text>
+    </View>
+  </View>
+);
+
+export default Meta;
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
+import { human, iOSColors } from 'react-native-typography';
+import Touchable from '@appandflow/touchable';
 
 import Header from './Header';
 import ActionButtons from './ActionButtons';
+import Meta from './Meta';
+import { CommentInput } from '../TextInputs';
 
 const styles = StyleSheet.create({
   root: {
@@ -11,6 +15,24 @@ const styles = StyleSheet.create({
   },
   img: {
     flex: 1,
+  },
+  commentsWrapper: {
+    height: 50,
+    paddingHorizontal: 16,
+  },
+  viewAllComments: {
+    ...human.calloutObject,
+    color: iOSColors.midGray,
+  },
+  timeAgoWrapper: {
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingHorizontal: 16,
+  },
+  timeAgo: {
+    ...human.footnoteObject,
+    color: iOSColors.midGray,
   },
 });
 
@@ -25,6 +47,16 @@ const PhotoCard = () => (
       }}
     />
     <ActionButtons />
+    <Meta />
+    <View style={styles.commentsWrapper}>
+      <Touchable feedback="opacity">
+        <Text style={styles.viewAllComments}>View all 13 comments</Text>
+      </Touchable>
+      <CommentInput />
+    </View>
+    <View style={styles.timeAgoWrapper}>
+      <Text style={styles.timeAgo}>6 HOURS AGO</Text>
+    </View>
   </View>
 );
 
